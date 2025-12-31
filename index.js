1950fc30f945a102386d9148429f639c04b194c8
fonemsanatmerkezi <fonemsanatmerkezi@gmail.com>
2025-12-31 17:05:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const sound = document.getElementById("clickSound");
 
+document.querySelectorAll(".slice").forEach(btn=>{
+  btn.addEventListener("click",()=>{
+    sound.currentTime = 0;
+    sound.play();
+  });
+});
