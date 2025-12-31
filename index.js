7a13f7eec8d5f5fb1d5ba6a580cdc7a5f0afa9d2
fonemsanatmerkezi <fonemsanatmerkezi@gmail.com>
2025-12-31 18:47:10 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const sound = document.getElementById("clickSound");
-
-document.querySelectorAll(".slice").forEach(btn=>{
-  btn.addEventListener("click",()=>{
-    sound.currentTime = 0;
-    sound.play();
-  });
-});
